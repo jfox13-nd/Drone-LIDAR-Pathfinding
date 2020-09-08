d996e5fe97cbe4df57daaa5ee56161ca93ad129e
Sean Crotty <scrotty1@nd.edu>
2020-09-08 18:37:50 -0400

    coords now center of circles

--- path.py
+++ path.py
@@ -5,9 +5,9 @@ root = tk.Tk()
 
 WIDTH=800
 HEIGHT=800
-D1 = 20
-D2 = 30
-D3 = 40
+R1 = 10
+R2 = 15
+R3 = 20
 
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
 
@@ -16,21 +16,26 @@ canvas.pack()
 text = canvas.create_text(400, 10, text="Keys to move = a,d,w,s")
 
 # Placing obstacles on the canvas
+# Oval 1 radius of 10 
 x1 = 680
 y1 = 680
-oval1 = canvas.create_oval(x1, y1, x1 + D1, y1 + D1, fill="yellow")
+oval1 = canvas.create_oval(x1 - R1, y1 - R1, x1 + R1, y1 + R1, fill="yellow")
+# Oval 2 radius of 10
 x2 = 620
 y2 = 660
-oval2 = canvas.create_oval(x2, y2, x2 + D1, y2 + D1, fill="yellow")
+oval2 = canvas.create_oval(x2 - R1, y2 - R1, x2 + R1, y2 + R1, fill="yellow")
+# Oval 3 radius of 15
 x3 = 450
 y3 = 500
-oval3 = canvas.create_oval(x3, y3, x3 + D2, y3 + D2, fill="yellow")
+oval3 = canvas.create_oval(x3 - R2, y3 - R2, x3 + R2, y3 + R2, fill="yellow")
+# Oval 4 radius of 15
 x4 = 600
 y4 = 250
-oval4 = canvas.create_oval(x4, y4, x4 + D2, y4 + D2, fill="yellow")
+oval4 = canvas.create_oval(x4 - R2, y4 - R2, x4 + R2, y4 + R2, fill="yellow")
+# Oval 5 radius of 20
 x5 = 705
 y5 = 320
-oval5 = canvas.create_oval(x5, y5, x5 + D3, y5 + D3, fill="yellow")
+oval5 = canvas.create_oval(x5- R3, y5 - R3, x5 + R3, y5 + R3, fill="yellow")
 
 # Placing the bot on the canvas
 bot_x = 400
